068a7a4a71fdd7c2246d82370b6dd32b863b33e6
veronikadumalo <dumalo44084@gmail.com>
2023-10-26 02:23:00 +0200

    fix bug

--- src/schema.ts
+++ src/schema.ts
@@ -3,5 +3,6 @@ import "./models/Deparments";
 import "./models/Person";
 import "./models/News";
 import "./models/Contact";
+import "./models/HomePageContent";
 
 export const schema = builder.toSchema({});
